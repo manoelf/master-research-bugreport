9b331d85c75cc15685acb161d345fda1a51e9b74
Lorenna Victoria <lorenna.lucena@ccc.ufcg.edu.br>
2023-07-19 12:45:04 -0300

    add contante

---
+++ src/modules/util/constant.py
@@ -0,0 +1,19 @@
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+
+class Features:
+
+    features = ["type", "flags", "assigned_to", "creator", "description", "summary", "id", "creation_time", "last_change_time", "resolution"]
+    summary = "summary"
+    description = "description"
+
+
+class Models:
+
+    NB = GaussianNB()
+    LG = LogisticRegression(max_iter=5000)
+    DT = DecisionTreeClassifier()
+    RF = RandomForestClassifier(random_state=42)
+    GB = GradientBoostingClassifier(random_state=0)
