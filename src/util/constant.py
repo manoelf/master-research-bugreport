fb643434a63263663f0db72d6afdd1d5f3939c5b
Lorenna Victoria <lorenna.lucena@ccc.ufcg.edu.br>
2023-07-19 12:45:29 -0300

    change sklearn plot function

--- src/util/constant.py
+++
@@ -1,7 +0,0 @@
-
-
-class Features:
-
-    features = ["type", "flags", "assigned_to", "creator", "description", "summary", "id", "creation_time", "last_change_time", "resolution"]
-    summary = "summary"
-    description = "description"
