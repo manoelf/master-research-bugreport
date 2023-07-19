dee6e4c2587c35ec47e498a02ce534c75ba6060c
Your Name <lorenna.lucena@ccc.ufcg.edu.br>
2023-07-19 00:47:12 -0300

    add helpers

---
+++ src/util/constant.py
@@ -0,0 +1,7 @@
+
+
+class Features:
+
+    features = ["type", "flags", "assigned_to", "creator", "description", "summary", "id", "creation_time", "last_change_time", "resolution"]
+    summary = "summary"
+    description = "description"
