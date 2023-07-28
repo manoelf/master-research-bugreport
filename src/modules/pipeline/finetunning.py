8361ff1452c6c9f9b2b447c331b92693d462c4d4
Lorenna Victoria <lorenna.lucena@ccc.ufcg.edu.br>
2023-07-27 21:15:32 -0300

    finishing pipeline notebook

--- src/modules/pipeline/finetunning.py
+++ src/modules/pipeline/finetunning.py
@@ -1,4 +1,4 @@
-from src.modules.util.util import Util as util
+from src.modules.util.util import DataUtil as util
 from src.modules.util.constant import Model, ModelName
 from modules.pipeline.training import get_classification_artifacts
 import pickle
@@ -9,9 +9,10 @@ class Finetunning:
         pass
 
 
-    def model_finetuning(self, model: Model, model_name: ModelName, train, test, model_turned_metrics_path, model_pred_path):
-        x_train, y_train, x_test, y_test, classes = get_classification_artifacts(train, test)
-        model_tuned_metrics, model_pred = util.get_tuned_metrics(model, model_name, 10, x_train, y_train, x_test, y_test, classes)
+    def model_finetuning(self, model: Model, model_name: ModelName, x_train, y_train, x_test, y_test, classes, model_turned_metrics_path, model_pred_path, folds=10):
+        model_tuned_metrics, model_pred = util.get_tuned_metrics(model, model_name, folds, x_train, y_train, x_test, y_test, classes)
         
         pickle.dump(model_tuned_metrics, open(model_turned_metrics_path, 'wb'))
         pickle.dump(model_pred, open(model_pred_path, 'wb'))
+
+        return model_tuned_metrics, model_pred
